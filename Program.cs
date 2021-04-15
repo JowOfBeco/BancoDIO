37710d0dfe6692ecd2bf9df7d20b8b09e8ff77a7
Johmself <jow_t22@hotmail.com>
2021-04-15 15:22:12 -0300

    Just a few update in main program.cs

--- Program.cs
+++ Program.cs
@@ -5,15 +5,9 @@ namespace BankTransfer
     class Program
     {
         static void Main(string[] args)
-        {   
-            
-            Conta inumerasContas = new Conta(TipoConta.PessoaFisica, 0, 0, "Johmself");
+        {              
             Conta operacao = new Conta();
-
-            operacao.Operacao();
-
-            
-
+            operacao.Operacao();           
         }
     }
 }
